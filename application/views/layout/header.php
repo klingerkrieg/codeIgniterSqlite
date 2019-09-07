093c5f82816a498326c99d205d985b46da509658
Alan Klinger <klingerkrieg@gmail.com>
2019-09-07 12:35:29 -0300

    No básico não existe menu.

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -11,19 +11,5 @@
 </head>
 <body>
 
-<?php if (isset($_SESSION['email'])): ?>
-
-<div class="ui top fixed menu">
-  <div class="item">
-    <img src="<?=base_url()?>/static/logo.png">
-  </div>
-  <a href="<?=site_url()?>/usuarios/" class="item">Usuários</a>
-  <a href="<?=site_url()?>/setores/" class="item">Setores</a>
-  <a href="<?=site_url()?>/grupos/" class="item">Grupos</a>
-  <a href="<?=site_url()?>/login/logout/" class="item"><?=$_SESSION['email']?> | Logout</a>
-
-</div>
-
-<?php endif; ?>
 
 <div class="ui text container">
